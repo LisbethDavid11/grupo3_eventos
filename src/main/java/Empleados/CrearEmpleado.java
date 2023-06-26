62c05b2e7b5dea59e28332370cf05c0ada3c356a
LisbethDavid11 <ldavid@unah.hn>
2023-06-25 19:19:48 -0600

    Editar registro de empleados

--- src/main/java/Empleados/CrearEmpleado.java
+++ src/main/java/Empleados/CrearEmpleado.java
@@ -18,27 +18,30 @@ import java.util.Objects;
 
 public class CrearEmpleado extends JFrame{
 
-    private JTextField campoNombres;
-    private JTextField campoApellidos;
-    private JTextField campoEdad;
-    private JTextField campoDireccion;
-    private JButton cancelarButton;
-    private JButton guardarButton;
+    public JTextField campoNombres;
+    public JTextField campoApellidos;
+    public JTextField campoEdad;
+    public JTextField campoDireccion;
+    public JButton cancelarButton;
+    public JButton guardarButton;
     private JPanel panel1;
-    private JFormattedTextField campoIdentidad;
-    private JRadioButton femeninoRadioButton;
-    private JRadioButton masculinoRadioButton;
-    private JTextField campoCorreo;
-    private JTextField campoTelefono;
-    private JTextField campoContacto;
-    private JRadioButton temporalRadioButton;
-    private JRadioButton permanenteRadioButton;
-    private JTextField campoNombreContacto;
+    public JFormattedTextField campoIdentidad;
+    public JRadioButton femeninoRadioButton;
+    public JRadioButton masculinoRadioButton;
+    public JTextField campoCorreo;
+    public JTextField campoTelefono;
+    public JTextField campoContacto;
+    public JRadioButton temporalRadioButton;
+    public JRadioButton permanenteRadioButton;
+    public JTextField campoNombreContacto;
+    public JLabel lblID;
+    public JButton actualizarButton;
     private Conexion sql;
     private Connection mysql;
-    private CrearEmpleado crearEmpleado = this;
+    public CrearEmpleado crearEmpleado = this;
     public ButtonGroup grupogenero;
     public ButtonGroup grupoTipo;
+
 
     private JTextField[] campos = {campoIdentidad, campoNombres, campoApellidos, campoEdad, campoCorreo, campoTelefono, campoContacto, campoDireccion};
 
@@ -199,6 +202,85 @@ public class CrearEmpleado extends JFrame{
 
         });
 
+        //Accion del boton actualizar
+        actualizarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int validacion = 0;
+                    int contador = 0;
+                    String mensaje = "Faltó ingresar: \n";
+                    for (JTextField campo : campos) {
+
+                        if (Objects.equals( campo.getText().replaceAll("\\s+",""), "")){
+                            mensaje += Empleados.ColumnasDeTabla[contador] + "\n";
+                            validacion += 1;
+
+                        }
+                        contador += 1;
+
+                    }
+                    if (validacion > 0){
+                        JOptionPane.showMessageDialog(null,mensaje);
+                        return;
+                    }
+                    if (Integer.parseInt(campoEdad.getText()) <18 || Integer.parseInt(campoEdad.getText()) >60){
+                        JOptionPane.showMessageDialog(null,"Su edad no es válida");
+                        return;
+                    }
+                    if (campoTelefono.getText().charAt(0) == '1' || campoTelefono.getText().charAt(0) == '4' || campoTelefono.getText().charAt(0) == '5' || campoTelefono.getText().charAt(0) == '6' || campoTelefono.getText().charAt(0) == '7' || campoTelefono.getText().charAt(0) == '0'){
+                        JOptionPane.showMessageDialog(null,"Su número de teléfono no es válido");
+                        return;
+                    }
+                    if (campoIdentidad.getText().length() <15){
+                        JOptionPane.showMessageDialog(null,"Su número de identidad debe contener 15 digitos incluyendo guiones");
+                        return;
+                    }
+                    if (campoEdad.getText().length() <2){
+                        JOptionPane.showMessageDialog(null,"Su edad debe contener 2 digitos");
+                        return;
+                    }
+
+                    if (campoTelefono.getText().length() <8){
+                        JOptionPane.showMessageDialog(null,"Su número de teléfono debe contener 8 digitos");
+                        return;
+                    }
+                    if (campoContacto.getText().charAt(0) == '1' || campoContacto.getText().charAt(0) == '4' || campoContacto.getText().charAt(0) == '5' || campoContacto.getText().charAt(0) == '6' || campoContacto.getText().charAt(0) == '7' || campoContacto.getText().charAt(0) == '0'){
+                        JOptionPane.showMessageDialog(null,"Su número de teléfono no es válido");
+                        return;
+                    }
+                    if (campoContacto.getText().length() <8){
+                        JOptionPane.showMessageDialog(null,"Su número de teléfono debe contener 8 digitos");
+                        return;
+                    }
+                    if (!Empleados.ComprobarIdentidad(campoIdentidad.getText())){
+                        JOptionPane.showMessageDialog(null,"Identidad no válida" );
+                        return;
+                    }
+                    if (Empleados.ValidarCorreo(campoCorreo.getText())){
+                        JOptionPane.showMessageDialog(null,"Formato de correo inválido.\n ejemplo:luis@xxx.xxx");
+                        return;
+                    }
+
+
+
+                    ActualizarDatos();
+                } catch (SQLException ex) {
+
+                    throw new RuntimeException(ex);
+                }
+                try {
+                    VistaEmpleado vistaEmpleado = new VistaEmpleado();
+                    vistaEmpleado.setVisible(true);
+                    crearEmpleado.dispose();
+
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+            }
+
+        });
     }
 
     //Consulta insertar
@@ -221,6 +303,26 @@ public class CrearEmpleado extends JFrame{
        preparedStatement.execute();
 
     }
+
+        //metodo Actualizar datos
+        public void ActualizarDatos() throws SQLException {
+            sql = new Conexion();
+            mysql = sql.conectamysql();
+            PreparedStatement preparedStatement = mysql.prepareStatement("UPDATE Empleados SET Identidad=?, Nombres=?, Apellidos=?, Genero=?, Edad=?, Correo=?, Telefono=?, nombreContactoDeEmergencia=?, ContactoDeEmergencia=?, Direccion=?, TipoDeEmpleado=? WHERE idEmpleados=?");
+            preparedStatement.setString(1,campoIdentidad.getText());
+            preparedStatement.setString(2,campoNombres.getText());
+            preparedStatement.setString(3,campoApellidos.getText());
+            preparedStatement.setString(4,femeninoRadioButton.isSelected()?"Femenino":"Masculino");
+            preparedStatement.setString(5,campoEdad.getText());
+            preparedStatement.setString(6,campoCorreo.getText());
+            preparedStatement.setString(7,campoTelefono.getText());
+            preparedStatement.setString(8,campoNombreContacto.getText());
+            preparedStatement.setString(9,campoContacto.getText());
+            preparedStatement.setString(10,campoDireccion.getText());
+            preparedStatement.setString(11,temporalRadioButton.isSelected()?"Temporal":"Permanente");
+            preparedStatement.setString(12,lblID.getText());
+            preparedStatement.execute();
+        }
     private void createUIComponents() {
         // TODO: place custom component creation code here
     }
--- src/main/java/Empleados/VistaEmpleado.java
+++ src/main/java/Empleados/VistaEmpleado.java
@@ -5,6 +5,7 @@ import Objetos.Conexion;
 import Objetos.Empleados;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -24,15 +25,17 @@ public class VistaEmpleado extends JFrame {
     private JTextField barrabusqueda;
     private JButton botonatras;
     private JButton botonadelante;
+    private JButton editarEmpleadoButton;
     private int intervalo = 0;
     private Conexion sql;
     private Connection mysql;
     private VistaEmpleado vistaEmpleado = this;
     private List<Empleados> ListaEmpleados = new ArrayList<>();
+    public String dato0,dato1,dato2,dato3,dato4,dato5,dato6,dato7,dato8,dato9,dato10,dato11;
 
 
 
-  public VistaEmpleado() throws SQLException {
+    public VistaEmpleado() throws SQLException {
       super("Lista De Empleados");
       setSize(600,600);
       setLocationRelativeTo(null);
@@ -107,43 +110,120 @@ public class VistaEmpleado extends JFrame {
       });
 
 
-  }
+      //Accion del boton editar
+
+        editarEmpleadoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int filaseleccionada;
+                    filaseleccionada = table1.getSelectedRow();
+                    if (filaseleccionada == -1) {
+                        JOptionPane.showMessageDialog(null, "No se ha seleccionado ninguna fila");
+                    } else {
+                        CrearEmpleado editarEmpleado = new CrearEmpleado();
+                        editarEmpleado.setVisible(true);
+                        vistaEmpleado.dispose();
+                        editarEmpleado.guardarButton.setVisible(false);
+
+                        String id = table1.getValueAt(filaseleccionada, 0).toString();
+                        Conexion objCon = new Conexion();
+                        Connection conn = objCon.conectamysql();
+                        try {
+                            PreparedStatement stmtr = conn.prepareStatement("select * from Empleados where eventos.empleados.idEmpleados="+id);
+                            ResultSet rsr = stmtr.executeQuery();
+
+                            if (rsr.next()) {
+                                dato0 = rsr.getString("idEmpleados");
+                                dato1 = rsr.getString("Identidad");
+                                dato2 = rsr.getString("Nombres");
+                                dato3 = rsr.getString("Apellidos");
+                                dato4 = rsr.getString("Genero");
+                                dato5 = rsr.getString("Edad");
+                                dato6 = rsr.getString("Correo");
+                                dato7 = rsr.getString("Telefono");
+                                dato8 = rsr.getString("NombreContactoDeEmergencia");
+                                dato9 = rsr.getString("ContactoDeEmergencia");
+                                dato10 = rsr.getString("Direccion");
+                                dato11 = rsr.getString("TipoDeEmpleado");
+                            }
+                            ;
+                            stmtr.close();
+                            rsr.close();
+                            conn.close();
+                        } catch (Exception eu) {
+                            eu.printStackTrace();
+                        }
+
+                        editarEmpleado.lblID.setText(dato0);
+                        editarEmpleado.campoIdentidad.setText(dato1);
+                        editarEmpleado.campoNombres.setText(dato2);
+                        editarEmpleado.campoApellidos.setText(dato3);
+                        if (dato4=="Femenino"){
+                            editarEmpleado.femeninoRadioButton.setSelected(true);
+                            editarEmpleado.masculinoRadioButton.setSelected(false);
+                        }else {
+                            editarEmpleado.femeninoRadioButton.setSelected(false);
+                            editarEmpleado.masculinoRadioButton.setSelected(true);
+                        }
+                        editarEmpleado.campoEdad.setText(dato5);
+                        editarEmpleado.campoCorreo.setText(dato6);
+                        editarEmpleado.campoTelefono.setText(dato7);
+                        editarEmpleado.campoNombreContacto.setText(dato8);
+                        editarEmpleado.campoContacto.setText(dato9);
+                        editarEmpleado.campoDireccion.setText(dato10);
+                        if (dato11=="Temporal"){
+                            editarEmpleado.temporalRadioButton.setSelected(true);
+                            editarEmpleado.permanenteRadioButton.setSelected(false);
+                        }else {
+                            editarEmpleado.permanenteRadioButton.setSelected(false);
+                            editarEmpleado.temporalRadioButton.setSelected(true);
+                        }
+                    }
+                } catch (HeadlessException ex) {
+                    JOptionPane.showMessageDialog(null, "Error: " + ex + "\nInténtelo nuevamente",
+                            " .::Error En la Operacion::.", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+    }
 
 
-  //consulta barra de busqueda
-  public ModeloDeEmpleado CargarDatos() throws SQLException {
-      sql = new Conexion();
-      mysql = sql.conectamysql();
-      PreparedStatement preparedStatement = mysql.prepareStatement("select * from " + Empleados.nombreDeTabla + " where Identidad like concat('%',?,'%') or Nombres like concat('%',?,'%') or Apellidos like concat('%',?,'%') limit ?, 10");
-      preparedStatement.setString(1,barrabusqueda.getText());
-      preparedStatement.setString(2,barrabusqueda.getText());
-      preparedStatement.setString(3,barrabusqueda.getText());
-      preparedStatement.setInt(4,intervalo);
-      ResultSet resultado = preparedStatement.executeQuery();
 
-      while (resultado.next()){
-          Empleados empleados = new Empleados();
-          empleados.setId(resultado.getInt(1));
-          empleados.setIdentidad(resultado.getString(2));
-          empleados.setNombres(resultado.getString(3));
-          empleados.setApellidos(resultado.getString(4));
-          empleados.setGenero(resultado.getString(5));
-          empleados.setEdad(resultado.getInt(6));
-          empleados.setCorreo(resultado.getString(7));
-          empleados.setTelefono(resultado.getString(8));
-          empleados.setContacto(resultado.getString(9));
-          empleados.setDireccion(resultado.getString(10));
-          empleados.setTipoDeEmpleado(resultado.getString(11));
+      //consulta barra de busqueda
+      public ModeloDeEmpleado CargarDatos() throws SQLException {
+          sql = new Conexion();
+          mysql = sql.conectamysql();
+          PreparedStatement preparedStatement = mysql.prepareStatement("select * from " + Empleados.nombreDeTabla + " where Identidad like concat('%',?,'%') or Nombres like concat('%',?,'%') or Apellidos like concat('%',?,'%') limit ?, 10");
+          preparedStatement.setString(1,barrabusqueda.getText());
+          preparedStatement.setString(2,barrabusqueda.getText());
+          preparedStatement.setString(3,barrabusqueda.getText());
+          preparedStatement.setInt(4,intervalo);
+          ResultSet resultado = preparedStatement.executeQuery();
 
-          ListaEmpleados.add(empleados);
+          while (resultado.next()){
+              Empleados empleados = new Empleados();
+              empleados.setId(resultado.getInt(1));
+              empleados.setIdentidad(resultado.getString(2));
+              empleados.setNombres(resultado.getString(3));
+              empleados.setApellidos(resultado.getString(4));
+              empleados.setGenero(resultado.getString(5));
+              empleados.setEdad(resultado.getInt(6));
+              empleados.setCorreo(resultado.getString(7));
+              empleados.setTelefono(resultado.getString(8));
+              empleados.setContacto(resultado.getString(9));
+              empleados.setDireccion(resultado.getString(10));
+              empleados.setTipoDeEmpleado(resultado.getString(11));
+
+              ListaEmpleados.add(empleados);
+          }
+          return new ModeloDeEmpleado(Empleados.Columnas,ListaEmpleados);
       }
-      return new ModeloDeEmpleado(Empleados.Columnas,ListaEmpleados);
-  }
-
-
 
 
     private void createUIComponents(){
 
   }
+
 }
