4e23b19fbd581d2b9e51a64f71e7a919b400222d
DaniaRuiz <dania.ruiz@unah.hn>
2023-06-27 22:02:55 -0600

    dania

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import Empleados.VistaEmpleado;
 import java.sql.SQLException;
 
 public class Main {
+    //dania
     public static void main(String[] args) throws SQLException {
         SubMenu menu = new SubMenu();
         menu.setVisible(true);
