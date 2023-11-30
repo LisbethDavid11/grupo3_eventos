6b2aa681a31afeb53d5da34215e8691ff4d150f4
LisbethDavid11 <ldavid@unah.hn>
2023-11-30 12:15:32 -0600

    Corrección de usuario

--- src/main/java/Login/SignUp.java
+++ src/main/java/Login/SignUp.java
@@ -443,7 +443,7 @@ public class SignUp extends JFrame {
             String[] imagenes = {"imagen 1.jpg", "imagen 2.jpg", "imagen 3.jpg", "imagen 4.jpg", "imagen 5.jpg", "imagen 6.jpg", "imagen 7.jpg", "imagen 8.jpg", "imagen 9.jpg", "imagen 10.jpg"};
             String imagenSeleccionada = imagenes[new Random().nextInt(imagenes.length)];
 
-            String query = "INSERT INTO usuarios (nombre, correo, contrasena, imagen) VALUES (?, ?, ?, ?, ?)";
+            String query = "INSERT INTO usuarios (nombre, correo, contrasena, imagen, rol_id) VALUES (?, ?, ?, ?, ?)";
             try (PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
                 preparedStatement.setString(1, nombre);
                 preparedStatement.setString(2, correo);
