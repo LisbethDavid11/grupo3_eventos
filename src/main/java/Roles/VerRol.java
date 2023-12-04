bbc163eac9f5e486e28f4a7286a34068a387e698
LisbethDavid11 <ldavid@unah.hn>
2023-12-03 20:49:59 -0600

    Ver rol (corrección)

--- src/main/java/Roles/VerRol.java
+++ src/main/java/Roles/VerRol.java
@@ -1,71 +1,184 @@
 package Roles;
+
 import Login.SesionUsuario;
 import Objetos.Conexion;
 
 import javax.swing.*;
-import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class VerRol extends JFrame {
+public class VerRol extends JFrame{
+    private JTextField campoNombre;
+    private JTextArea campoDomicilio;
+    private JButton volverButton;
+    private JPanel panel, panel1;
+    private JLabel label0, label1, label2;
+    private VerRol actual = this;
+    private Connection mysql;
     private Conexion sql;
-    private int id;
-    private String nombreRol;
-    private String descripcion;
+    public VerRol verRol = this;
+    Color darkColorRed = new Color(244, 67, 54);
+    Color darkColorBlue = new Color(33, 150, 243);
 
+    // Color de texto para los JTextField y JRadioButton
+    Color textColor = Color.decode("#212121");
+    Font fontTitulo = new Font("Century Gothic", Font.BOLD, 20);
+    Font font = new Font("Century Gothic", Font.BOLD, 17);
+    Font font2 = new Font("Century Gothic", Font.BOLD, 11);
+
+    // Colores para el botón "Cyan"
+    Color primaryColorCyan = new Color(0, 188, 212); // Cyan primario
+    Color lightColorCyan = new Color(77, 208, 225); // Cyan claro
+    Color darkColorCyan = new Color(0, 151, 167); // Cyan oscuro
+
+    // Colores para el botón "Aqua"
+    Color primaryColorAqua = new Color(0, 150, 136); // Aqua primario
+    Color lightColorAqua = new Color(77, 182, 172); // Aqua claro
+    Color darkColorAqua = new Color(0, 121, 107); // Aqua oscuro
+
+    // Colores para el botón "Rosado"
+    Color primaryColorRosado = new Color(233, 30, 99); // Rosado primario
+    Color lightColorRosado = new Color(240, 98, 146); // Rosado claro
+    Color darkColorRosado = new Color(194, 24, 91); // Rosado oscuro
+
+    // Crea un margen de 10 píxeles desde el borde inferior
+    EmptyBorder margin = new EmptyBorder(15, 0, 15, 0);
+
+    int id;
     public VerRol(int id) {
         super("");
-        setSize(505, 300);
+        setSize(450,350);
         setLocationRelativeTo(null);
+        setContentPane(panel);
         sql = new Conexion();
         this.id = id;
+        mostrarRol();
 
-        setLayout(new BorderLayout(0, 0));
+        campoDomicilio.setLineWrap(true);
+        campoDomicilio.setWrapStyleWord(true);
 
-        // Panel para Título y Nombre del Rol
-        JPanel tituloPanel = new JPanel();
-        tituloPanel.setLayout(new BoxLayout(tituloPanel, BoxLayout.Y_AXIS));
-        tituloPanel.setBackground(new Color(245,245,245));
+        panel.setBackground(Color.decode("#F5F5F5"));
+        panel1.setBackground(Color.decode("#F5F5F5"));
 
-        JLabel titleLabel = new JLabel("VISUALIZAR DATOS DEL ROL", JLabel.CENTER);
-        titleLabel.setFont(new Font("Century Gothic", Font.BOLD, 30));
-        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        titleLabel.setForeground(Color.decode("#2196F3"));
-        tituloPanel.add(titleLabel);
+        campoNombre.setEditable(false);
+        campoNombre.setFocusable(false);
 
-        JLabel nombreLabel = new JLabel(); // Se establecerá en mostrarRol()
-        nombreLabel.setFont(new Font("Century Gothic", Font.BOLD, 20));
-        nombreLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        tituloPanel.add(nombreLabel);
+        campoDomicilio.setEditable(false);
+        campoDomicilio.setFocusable(false);
 
-        add(tituloPanel, BorderLayout.NORTH);
+        campoNombre.setBorder(BorderFactory.createEmptyBorder());
+        campoDomicilio.setBorder(BorderFactory.createEmptyBorder());
 
-        // Panel para los permisos
-        JPanel permisosPanel = new JPanel();
-        permisosPanel.setLayout(new BoxLayout(permisosPanel, BoxLayout.Y_AXIS));
-        permisosPanel.setBackground(new Color(245, 245, 245));
-        permisosPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        add(permisosPanel, BorderLayout.CENTER);
+        campoNombre.setFont(font);
+        campoDomicilio.setFont(font);
 
-        JTextArea descripcionTextArea = new JTextArea();
-        descripcionTextArea.setFont(new Font("Century Gothic", Font.BOLD, 18));
-        descripcionTextArea.setAlignmentX(Component.CENTER_ALIGNMENT);
-        descripcionTextArea.setOpaque(false);
-        descripcionTextArea.setLineWrap(true); // Para que el texto se ajuste automáticamente al ancho
-        descripcionTextArea.setWrapStyleWord(true); // Para que las palabras se rompan correctamente
-        descripcionTextArea.setEditable(false);
-        descripcionTextArea.setFocusable(false);
-        permisosPanel.add(descripcionTextArea);
+        Color textColor = Color.decode("#263238");
+        Color textColor2 = Color.decode("#607d8b");
+        campoNombre.setForeground(textColor);
+        campoDomicilio.setForeground(textColor);
 
-        // Botón para cerrar la ventana
-        JButton closeButton = new JButton("Cerrar");
-        styleMaterialButton(closeButton);
-        closeButton.addActionListener(new ActionListener() {
+        Color textFieldColor = Color.decode("#F5F5F5");
+        campoNombre.setBackground(textFieldColor);
+        campoDomicilio.setBackground(textFieldColor);
+
+        volverButton.setForeground(Color.WHITE);
+        volverButton.setBackground(Color.decode("#263238"));
+        volverButton.setFocusPainted(false);
+        volverButton.setBorder(margin);
+
+        label0.setBorder(margin);
+        label0.setFont(fontTitulo);
+
+        label1.setForeground(textColor2);
+        label2.setForeground(textColor2);
+
+        label1.setFont(font2);
+        label2.setFont(font2);
+
+        volverButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                volverButton.setBackground(textColor2);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                volverButton.setBackground(textColor);
+            }
+        });
+
+        campoNombre.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                String text = campoNombre.getText();
+                int length = text.length();
+                int caretPosition = campoNombre.getCaretPosition();
+
+                // Verificar si se está ingresando un espacio en blanco
+                if (e.getKeyChar() == ' ') {
+                    // Verificar si es el primer espacio en blanco o si hay varios espacios consecutivos
+                    if (length == 0 || caretPosition == 0 || text.charAt(caretPosition - 1) == ' ') {
+                        e.consume(); // Ignorar el espacio en blanco adicional
+                    }
+                } else {
+                    // Verificar la longitud del texto después de eliminar espacios en blanco
+                    String trimmedText = text.replaceAll("\\s+", " ");
+                    int trimmedLength = trimmedText.length();
+
+                    // Verificar si se está ingresando una letra
+                    if (Character.isLetterOrDigit(e.getKeyChar())) {
+                        // Verificar si se excede el límite de caracteres
+                        if (trimmedLength >= 100) {
+                            e.consume(); // Ignorar la letra
+                        } else {
+                            // Convertir solo la primera letra a mayúscula
+                            if (caretPosition == 0) {
+                                e.setKeyChar(Character.toUpperCase(e.getKeyChar()));
+                            }
+                        }
+                    } else {
+                        e.consume(); // Ignorar cualquier otro tipo de carácter
+                    }
+                }
+            }
+        });
+
+        campoDomicilio.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                String texto = campoDomicilio.getText();
+                int caretPosition = campoDomicilio.getCaretPosition();
+
+                // Verificar la longitud del texto
+                if (texto.length() >= 200) {
+                    e.consume(); // Ignorar el evento si se alcanza el límite máximo de caracteres (200)
+                    return;
+                }
+
+                // Verificar si se están ingresando más de un espacio en blanco seguido
+                if (e.getKeyChar() == ' ' && texto.endsWith(" ")) {
+                    e.consume(); // Ignorar el evento y no agregar el espacio en blanco adicional
+                    return;
+                }
+
+                // Convertir la primera letra en mayúscula
+                if (texto.length() == 0 && Character.isLowerCase(e.getKeyChar())) {
+                    e.setKeyChar(Character.toUpperCase(e.getKeyChar()));
+                }
+
+                // Permitir números, letras, espacios, punto, coma y tildes
+                if (!Character.isLetterOrDigit(e.getKeyChar()) && !Character.isSpaceChar(e.getKeyChar()) && e.getKeyChar() != '.' && e.getKeyChar() != ',' && !Character.isWhitespace(e.getKeyChar()) && !Character.isIdeographic(e.getKeyChar())) {
+                    e.consume(); // Ignorar el evento si no es una letra, número, espacio, punto, coma o tilde
+                }
+            }
+        });
+
+        volverButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int idUsuarioActual = SesionUsuario.getInstance().getIdUsuario();
@@ -74,15 +187,11 @@ public class VerRol extends JFrame {
                 dispose();
             }
         });
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setBackground(new Color(149, 165, 166)); // Fondo gris detrás del botón
-        buttonPanel.add(closeButton);
-        add(buttonPanel, BorderLayout.SOUTH);
 
-        mostrarRol(nombreLabel, descripcionTextArea);
     }
 
-    private void mostrarRol(JLabel nombreLabel, JTextArea descripcionTextArea) {
+
+    private void mostrarRol() {
         try (Connection connection = sql.conectamysql();
              PreparedStatement preparedStatement = connection.prepareStatement(
                      "SELECT * FROM roles WHERE id = ?")) {
@@ -90,11 +199,8 @@ public class VerRol extends JFrame {
             ResultSet rs = preparedStatement.executeQuery();
 
             if (rs.next()) {
-                nombreRol = rs.getString("nombre");
-                nombreLabel.setText(nombreRol);
-                descripcion = rs.getString("descripcion");
-                descripcionTextArea.setText(descripcion);
-
+                campoNombre.setText(rs.getString("nombre"));
+                campoDomicilio.setText(rs.getString("descripcion"));
             } else {
                 JOptionPane.showMessageDialog(this, "El rol con este nombre no existe.", "Error", JOptionPane.ERROR_MESSAGE);
             }
@@ -104,35 +210,114 @@ public class VerRol extends JFrame {
         }
     }
 
-    private void styleMaterialButton(JButton button) {
-        Color buttonColor = Color.decode("#2c3e50"); // Color principal para el fondo del botón
-        Color hoverColor = Color.decode("#34495e"); // Color para el fondo del botón al pasar el ratón por encima
 
-        button.setBackground(buttonColor);
-        button.setFont(new Font("Arial", Font.BOLD, 14));
-        button.setForeground(Color.WHITE);
-        button.setFocusPainted(false);
-        button.setBorder(BorderFactory.createEmptyBorder(10, 25, 10, 25)); // Espacio alrededor del texto del botón
-        // Sombra ligera para dar efecto de elevación
-        button.setBorder(BorderFactory.createCompoundBorder(
-                BorderFactory.createRaisedBevelBorder(),
-                BorderFactory.createEmptyBorder(10, 25, 10, 25)
-        ));
+    public void mostrarDialogoPersonalizadoExito(String mensaje, Color colorFondoBoton) {
+        // Crea un botón personalizado
+        JButton btnAceptar = new JButton("OK");
+        btnAceptar.setBackground(colorFondoBoton); // Color de fondo del botón
+        btnAceptar.setForeground(Color.WHITE);
+        btnAceptar.setFocusPainted(false);
 
-        // Cambia el color del botón al pasar el ratón por encima para simular elevación
-        button.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                button.setBackground(hoverColor); // Cambia al color de hover
-            }
+        // Crea un JOptionPane
+        JOptionPane optionPane = new JOptionPane(
+                mensaje,                           // Mensaje a mostrar
+                JOptionPane.INFORMATION_MESSAGE,   // Tipo de mensaje
+                JOptionPane.DEFAULT_OPTION,        // Opción por defecto (no específica aquí)
+                null,                              // Icono (puede ser null)
+                new Object[]{},                    // No se usan opciones estándar
+                null                               // Valor inicial (no necesario aquí)
+        );
 
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                button.setBackground(buttonColor); // Vuelve al color principal
+        // Añade el botón al JOptionPane
+        optionPane.setOptions(new Object[]{btnAceptar});
+
+        // Crea un JDialog para mostrar el JOptionPane
+        JDialog dialog = optionPane.createDialog("Validación");
+
+        // Añade un ActionListener al botón
+        btnAceptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dialog.dispose(); // Cierra el diálogo al hacer clic en "Aceptar"
             }
         });
+
+        // Muestra el diálogo
+        dialog.setVisible(true);
+    }
+
+    public void mostrarDialogoPersonalizadoError(String mensaje, Color colorFondoBoton) {
+        // Crea un botón personalizado
+        JButton btnAceptar = new JButton("OK");
+        btnAceptar.setBackground(colorFondoBoton); // Color de fondo del botón
+        btnAceptar.setForeground(Color.WHITE);
+        btnAceptar.setFocusPainted(false);
+
+        // Crea un JOptionPane
+        JOptionPane optionPane = new JOptionPane(
+                mensaje,                           // Mensaje a mostrar
+                JOptionPane.WARNING_MESSAGE,   // Tipo de mensaje
+                JOptionPane.DEFAULT_OPTION,        // Opción por defecto (no específica aquí)
+                null,                              // Icono (puede ser null)
+                new Object[]{},                    // No se usan opciones estándar
+                null                               // Valor inicial (no necesario aquí)
+        );
+
+        // Añade el botón al JOptionPane
+        optionPane.setOptions(new Object[]{btnAceptar});
+
+        // Crea un JDialog para mostrar el JOptionPane
+        JDialog dialog = optionPane.createDialog("Validación");
+
+        // Añade un ActionListener al botón
+        btnAceptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dialog.dispose(); // Cierra el diálogo al hacer clic en "Aceptar"
+            }
+        });
+
+        // Muestra el diálogo
+        dialog.setVisible(true);
+    }
+
+    public void mostrarDialogoPersonalizadoAtencion(String mensaje, Color colorFondoBoton) {
+        // Crea un botón personalizado
+        JButton btnAceptar = new JButton("OK");
+        btnAceptar.setBackground(colorFondoBoton); // Color de fondo del botón
+        btnAceptar.setForeground(Color.WHITE);
+        btnAceptar.setFocusPainted(false);
+
+        // Crea un JOptionPane
+        JOptionPane optionPane = new JOptionPane(
+                mensaje,                           // Mensaje a mostrar
+                JOptionPane.WARNING_MESSAGE,   // Tipo de mensaje
+                JOptionPane.DEFAULT_OPTION,        // Opción por defecto (no específica aquí)
+                null,                              // Icono (puede ser null)
+                new Object[]{},                    // No se usan opciones estándar
+                null                               // Valor inicial (no necesario aquí)
+        );
+
+        // Añade el botón al JOptionPane
+        optionPane.setOptions(new Object[]{btnAceptar});
+
+        // Crea un JDialog para mostrar el JOptionPane
+        JDialog dialog = optionPane.createDialog("Validación");
+
+        // Añade un ActionListener al botón
+        btnAceptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dialog.dispose(); // Cierra el diálogo al hacer clic en "Aceptar"
+            }
+        });
+
+        // Muestra el diálogo
+        dialog.setVisible(true);
     }
 
     public static void main(String[] args) {
-        VerRol verRol = new VerRol(1);
-        verRol.setVisible(true);
+        VerRol verRoles = new VerRol(1);
+        verRoles.setVisible(true);
     }
 }
